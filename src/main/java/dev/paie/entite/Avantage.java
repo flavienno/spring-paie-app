7c570510fa066905fffa6b35834b0ba51b79d923
Flavien Oulhiou-Naudin <flavien.naudin@gmail.com>
2020-01-20 17:25:39 +0100

    TP 02 first commit

---
+++ src/main/java/dev/paie/entite/Avantage.java
@@ -0,0 +1,58 @@
+package dev.paie.entite;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "avantage")
+public class Avantage {
+
+	@Id
+	@Column(name = "ID")
+	private Integer id;
+
+	@Column(name = "code")
+	private String code;
+
+	@Column(name = "nom")
+	private String nom;
+
+	@Column(name = "montant")
+	private BigDecimal montant;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public BigDecimal getMontant() {
+		return montant;
+	}
+
+	public void setMontant(BigDecimal montant) {
+		this.montant = montant;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
